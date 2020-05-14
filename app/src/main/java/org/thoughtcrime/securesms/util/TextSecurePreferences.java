e24853fabd29c8f68cf992b4eb70b841531f7bc2
Unknown <johanw@xs4all.nl>
2020-05-14 22:54:30 +0200

    Added encrypted and plaintext backup / restore. Added a choice for the backup location. Added an option to treat view-once media as normal media. Added choice for the maps type.

--- app/src/main/java/org/thoughtcrime/securesms/util/TextSecurePreferences.java
+++ app/src/main/java/org/thoughtcrime/securesms/util/TextSecurePreferences.java
@@ -190,6 +190,58 @@ public class TextSecurePreferences {
 
   public  static final String ENABLE_PINV2_REMINDERS = "pref_signal_enable_pinv2_reminders";
 
+  // JW: added: true = backup to removable SD card (if available), false = backup to internal sd card
+  public static final String BACKUP_LOCATION_REMOVABLE_PREF = "pref_backup_location_external";
+  // JW: added to use encrypted zipfiles to store raw backups
+  public static final String BACKUP_STORE_ZIPFILE_PREF = "pref_backup_zipfile";
+  // JW: added to use encrypted zipfiles to store plaintext backups
+  public static final String BACKUP_STORE_ZIPFILE_PLAIN_PREF = "pref_backup_zipfile_plain";
+  // JW: used to see if we delete view once messagres or not
+  public static final String KEEP_VIEW_ONCE_MESSAGES = "pref_keep_view_once_messages";
+  // JW: select map type for location picker
+  public static final String GOOGLE_MAP_TYPE = "pref_google_map_type";
+
+  // JW added
+  public static void setBackupLocationRemovable(@NonNull Context context, boolean value) {
+    setBooleanPreference(context, BACKUP_LOCATION_REMOVABLE_PREF, value);
+  }
+  // JW added. Default to false so default does the same as official Signal.
+  public static boolean isBackupLocationRemovable(@NonNull Context context) {
+    return getBooleanPreference(context, BACKUP_LOCATION_REMOVABLE_PREF, false);
+  }
+  // JW added.
+  public static boolean isRawBackupInZipfile(@NonNull Context context) {
+    return getBooleanPreference(context, BACKUP_STORE_ZIPFILE_PREF, false);
+  }
+  // JW added.
+  public static void setRawBackupZipfile(@NonNull Context context, boolean value) {
+    setBooleanPreference(context, BACKUP_STORE_ZIPFILE_PREF, value);
+  }
+  // JW added.
+  public static boolean isPlainBackupInZipfile(@NonNull Context context) {
+    return getBooleanPreference(context, BACKUP_STORE_ZIPFILE_PLAIN_PREF, false);
+  }
+  // JW added.
+  public static void setPlainBackupZipfile(@NonNull Context context, boolean value) {
+    setBooleanPreference(context, BACKUP_STORE_ZIPFILE_PLAIN_PREF, value);
+  }
+  // JW added.
+  public static boolean isKeepViewOnceMessages(@NonNull Context context) {
+    return getBooleanPreference(context, KEEP_VIEW_ONCE_MESSAGES, false);
+  }
+  // JW added.
+  public static void setKeepViewOnceMessages(@NonNull Context context, boolean value) {
+    setBooleanPreference(context, KEEP_VIEW_ONCE_MESSAGES, value);
+  }
+  // JW: added
+  public static String getGoogleMapType(Context context) {
+    return getStringPreference(context, GOOGLE_MAP_TYPE, "normal");
+  }
+  // JW: added
+  public static void setGoogleMapType(Context context, String value) {
+    setStringPreference(context, GOOGLE_MAP_TYPE, value);
+  }
+
   public static boolean isPinV2ReminderEnabled(@NonNull Context context) {
     return getBooleanPreference(context, ENABLE_PINV2_REMINDERS, true);
   }
