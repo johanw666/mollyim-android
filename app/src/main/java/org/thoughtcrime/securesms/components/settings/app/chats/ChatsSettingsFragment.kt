b683b488ef8791ee20979b44ca0c656fb195b6e8
Unknown <johanw@xs4all.nl>
2022-04-05 23:18:06 +0200

    Standard changes but crash after typing password

--- app/src/main/java/org/thoughtcrime/securesms/components/settings/app/chats/ChatsSettingsFragment.kt
+++ app/src/main/java/org/thoughtcrime/securesms/components/settings/app/chats/ChatsSettingsFragment.kt
@@ -1,18 +1,29 @@
 package org.thoughtcrime.securesms.components.settings.app.chats
 
+import android.app.Activity // JW: added
+import android.content.Intent // JW: added
+import android.os.Build // JW: added
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.preference.PreferenceManager // JW: added
 import org.thoughtcrime.securesms.R
+import org.thoughtcrime.securesms.backup.BackupDialog
 import org.thoughtcrime.securesms.components.settings.DSLConfiguration
 import org.thoughtcrime.securesms.components.settings.DSLSettingsAdapter
 import org.thoughtcrime.securesms.components.settings.DSLSettingsFragment
 import org.thoughtcrime.securesms.components.settings.DSLSettingsText
 import org.thoughtcrime.securesms.components.settings.configure
 import org.thoughtcrime.securesms.util.navigation.safeNavigate
+import org.thoughtcrime.securesms.keyvalue.SignalStore
+import org.thoughtcrime.securesms.preferences.BackupsPreferenceFragment
+import org.thoughtcrime.securesms.util.UriUtils // JW: added
+
 
 class ChatsSettingsFragment : DSLSettingsFragment(R.string.preferences_chats__chats) {
 
   private lateinit var viewModel: ChatsSettingsViewModel
+  
+  val CHOOSE_BACKUPS_LOCATION_REQUEST_CODE = 1201 // JW: added
 
   override fun onResume() {
     super.onResume()
@@ -21,11 +32,27 @@ class ChatsSettingsFragment : DSLSettingsFragment(R.string.preferences_chats__ch
 
   override fun bindAdapter(adapter: DSLSettingsAdapter) {
     val repository = ChatsSettingsRepository()
-    val factory = ChatsSettingsViewModel.Factory(repository)
+    val preferences = PreferenceManager.getDefaultSharedPreferences(context) // JW: added
+    val factory = ChatsSettingsViewModel.Factory(preferences, repository) // JW: added preferences
     viewModel = ViewModelProvider(this, factory)[ChatsSettingsViewModel::class.java]
 
     viewModel.state.observe(viewLifecycleOwner) {
       adapter.submitList(getConfiguration(it).toMappingModelList())
+    }
+  }
+
+  // JW: added
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+
+    if (data != null && data.data != null) {
+      if (resultCode == Activity.RESULT_OK) {
+        if (requestCode == CHOOSE_BACKUPS_LOCATION_REQUEST_CODE) {
+          val backupUri = data.data
+          SignalStore.settings().setSignalBackupDirectory(backupUri!!)
+          viewModel.setChatBackupLocationApi30(UriUtils.getFullPathFromTreeUri(context, backupUri))
+        }
+      }
     }
   }
 
@@ -79,6 +106,98 @@ class ChatsSettingsFragment : DSLSettingsFragment(R.string.preferences_chats__ch
         summary = DSLSettingsText.from(if (state.chatBackupsEnabled) R.string.arrays__enabled else R.string.arrays__disabled),
         onClick = {
           Navigation.findNavController(requireView()).safeNavigate(R.id.action_chatsSettingsFragment_to_backupsPreferenceFragment)
+        }
+      )
+
+      // JW: added
+      if (Build.VERSION.SDK_INT < 30) {
+        switchPref(
+          title = DSLSettingsText.from(R.string.preferences_chats__chat_backups_removable),
+          summary = DSLSettingsText.from(R.string.preferences_chats__backup_chats_to_removable_storage),
+          isChecked = state.chatBackupsLocation,
+          onClick = {
+            viewModel.setChatBackupLocation(!state.chatBackupsLocation)
+          }
+        )
+      } else {
+        val backupUri = SignalStore.settings().signalBackupDirectory
+        val summaryText = UriUtils.getFullPathFromTreeUri(context, backupUri)
+
+        clickPref(
+          title = DSLSettingsText.from(R.string.preferences_chats__chat_backups_removable),
+          summary = DSLSettingsText.from(summaryText),
+          onClick = {
+            BackupDialog.showChooseBackupLocationDialog(this@ChatsSettingsFragment, CHOOSE_BACKUPS_LOCATION_REQUEST_CODE)
+            viewModel.setChatBackupLocationApi30(UriUtils.getFullPathFromTreeUri(context, backupUri))
+          }
+        )
+      }
+
+      // JW: added
+      switchPref(
+        title = DSLSettingsText.from(R.string.preferences_chats__chat_backups_zipfile),
+        summary = DSLSettingsText.from(R.string.preferences_chats__backup_chats_to_encrypted_zipfile),
+        isChecked = state.chatBackupZipfile,
+        onClick = {
+          viewModel.setChatBackupZipfile(!state.chatBackupZipfile)
+        }
+      )
+
+      // JW: added
+      switchPref(
+        title = DSLSettingsText.from(R.string.preferences_chats__chat_backups_zipfile_plain),
+        summary = DSLSettingsText.from(R.string.preferences_chats__backup_chats_to_encrypted_zipfile_plain),
+        isChecked = state.chatBackupZipfilePlain,
+        onClick = {
+          viewModel.setChatBackupZipfilePlain(!state.chatBackupZipfilePlain)
+        }
+      )
+
+      dividerPref()
+
+      sectionHeaderPref(R.string.preferences_chats__control_message_deletion)
+
+      // JW: added
+      switchPref(
+        title = DSLSettingsText.from(R.string.preferences_chats__chat_keep_view_once_messages),
+        summary = DSLSettingsText.from(R.string.preferences_chats__keep_view_once_messages_summary),
+        isChecked = state.keepViewOnceMessages,
+        onClick = {
+          viewModel.keepViewOnceMessages(!state.keepViewOnceMessages)
+        }
+      )
+
+      // JW: added
+      switchPref(
+        title = DSLSettingsText.from(R.string.preferences_chats__chat_ignore_remote_delete),
+        summary = DSLSettingsText.from(R.string.preferences_chats__chat_ignore_remote_delete_summary),
+        isChecked = state.ignoreRemoteDelete,
+        onClick = {
+          viewModel.ignoreRemoteDelete(!state.ignoreRemoteDelete)
+        }
+      )
+
+      // JW: added
+      switchPref(
+        title = DSLSettingsText.from(R.string.preferences_chats__delete_media_only),
+        summary = DSLSettingsText.from(R.string.preferences_chats__delete_media_only_summary),
+        isChecked = state.deleteMediaOnly,
+        onClick = {
+          viewModel.deleteMediaOnly(!state.deleteMediaOnly)
+        }
+      )
+
+      dividerPref()
+
+      sectionHeaderPref(R.string.preferences_chats__group_control)
+
+      // JW: added
+      switchPref(
+        title = DSLSettingsText.from(R.string.preferences_chats__can_blocked_contacts_add_you_to_groups),
+        summary = DSLSettingsText.from(R.string.preferences_chats__can_blocked_contacts_add_you_to_groups_summary),
+        isChecked = state.blockedContactsCantAddYouToGroups,
+        onClick = {
+          viewModel.setBlockedCanAddYouToGroups(!state.blockedContactsCantAddYouToGroups)
         }
       )
     }
--- app/src/main/java/org/thoughtcrime/securesms/messages/MessageContentProcessor.java
+++ app/src/main/java/org/thoughtcrime/securesms/messages/MessageContentProcessor.java
@@ -938,12 +938,28 @@ public final class MessageContentProcessor {
     return new MessageId(targetMessage.getId(), targetMessage.isMms());
   }
 
+  // JW: set a reaction to a message. Thanks ClauZ for the implementation
+  private void setMessageReaction(@NonNull SignalServiceDataMessage message, @Nullable MessageRecord targetMessage, String reaction) {
+    if (targetMessage != null) {
+      String reactionEmoji = EmojiUtil.getCanonicalRepresentation(reaction);
+
+      MessageId      targetMessageId = new MessageId(targetMessage.getId(), targetMessage.isMms());
+      ReactionRecord reactionRecord  = new ReactionRecord(reactionEmoji, Recipient.self().getId(), message.getTimestamp(), System.currentTimeMillis());
+
+      SignalDatabase.reactions().addReaction(targetMessageId, reactionRecord);
+      ApplicationDependencies.getMessageNotifier().updateNotification(context, targetMessage.getThreadId(), false);
+    }
+  }
+
   private @Nullable MessageId handleRemoteDelete(@NonNull SignalServiceContent content, @NonNull SignalServiceDataMessage message, @NonNull Recipient senderRecipient) {
     log(content.getTimestamp(), "Remote delete for message " + message.getRemoteDelete().get().getTargetSentTimestamp());
 
     SignalServiceDataMessage.RemoteDelete delete = message.getRemoteDelete().get();
 
     MessageRecord targetMessage = SignalDatabase.mmsSms().getMessageFor(delete.getTargetSentTimestamp(), senderRecipient.getId());
+
+    // JW: set a reaction to indicate the message was remote deleted. Sender is myself, emoji is an exclamation.
+    if (TextSecurePreferences.isIgnoreRemoteDelete(context)) { setMessageReaction(message, targetMessage, "\u2757"); return null; }
 
     if (targetMessage != null && RemoteDeleteUtil.isValidReceive(targetMessage, senderRecipient, content.getServerReceivedTimestamp())) {
       MessageDatabase db = targetMessage.isMms() ? SignalDatabase.mms() : SignalDatabase.sms();
@@ -1314,6 +1330,7 @@ public final class MessageContentProcessor {
   }
 
   private void handleSynchronizeViewOnceOpenMessage(@NonNull ViewOnceOpenMessage openMessage, long envelopeTimestamp) {
+    if (TextSecurePreferences.isKeepViewOnceMessages(context)) return; // JW
     log(envelopeTimestamp, "Handling a view-once open for message: " + openMessage.getTimestamp());
 
     RecipientId   author    = Recipient.externalPush(openMessage.getSender()).getId();
@@ -1629,6 +1646,7 @@ public final class MessageContentProcessor {
     MessageDatabase database = SignalDatabase.mms();
     database.beginTransaction();
 
+    boolean viewOnce = TextSecurePreferences.isKeepViewOnceMessages(context) ? false : message.isViewOnce(); // JW
     try {
       Optional<QuoteModel>        quote          = getValidatedQuote(message.getQuote());
       Optional<List<Contact>>     sharedContacts = getContacts(message.getSharedContacts());
@@ -1646,7 +1664,7 @@ public final class MessageContentProcessor {
                                                                    -1,
                                                                    TimeUnit.SECONDS.toMillis(message.getExpiresInSeconds()),
                                                                    false,
-                                                                   message.isViewOnce(),
+                                                                   viewOnce, // JW
                                                                    content.isNeedsReceipt(),
                                                                    message.getBody(),
                                                                    message.getGroupContext(),
@@ -1687,7 +1705,12 @@ public final class MessageContentProcessor {
       ApplicationDependencies.getMessageNotifier().updateNotification(context, insertResult.get().getThreadId());
       TrimThreadJob.enqueueAsync(insertResult.get().getThreadId());
 
-      if (message.isViewOnce()) {
+      // JW: add a [1] reaction to indicate the message was viewOnce.
+      if (TextSecurePreferences.isKeepViewOnceMessages(context) && message.isViewOnce()) {
+        MessageRecord targetMessage = SignalDatabase.mmsSms().getMessageFor(message.getTimestamp(), senderRecipient.getId());
+        setMessageReaction(message, targetMessage, "\u0031\uFE0F\u20E3");
+      }
+      if (viewOnce) { // JW
         ApplicationDependencies.getViewOnceMessageManager().scheduleIfNecessary();
       }
 
@@ -1731,7 +1754,7 @@ public final class MessageContentProcessor {
     Optional<List<Contact>>     sharedContacts  = getContacts(message.getMessage().getSharedContacts());
     Optional<List<LinkPreview>> previews        = getLinkPreviews(message.getMessage().getPreviews(), message.getMessage().getBody().orElse(""), false);
     Optional<List<Mention>>     mentions        = getMentions(message.getMessage().getMentions());
-    boolean                     viewOnce        = message.getMessage().isViewOnce();
+    boolean                     viewOnce        = TextSecurePreferences.isKeepViewOnceMessages(context) ? false : message.getMessage().isViewOnce(); // JW
     List<Attachment>            syncAttachments = viewOnce ? Collections.singletonList(new TombstoneAttachment(MediaUtil.VIEW_ONCE, false))
         : PointerAttachment.forPointers(message.getMessage().getAttachments());
 
