b189d1ac3a9229af99651b0dbe75fe801cb98809
Johan <johanw@xs4all.nl>
2022-10-09 23:38:27 +0200

    Added extra options
    - Added encrypted and plaintext backup / restore.
    - Added WhatsApp import.
    - Option to prevent blocked users from adding you to a group.
    - Choice for the backup location.
    - Option to treat view-once media as normal media.
    - Option to ignore remote delete of messages.
    - Added an option to only delete media from a message, not the entire message.
    - Removed forward limit.
    - Show internal options. Use at your won risk.
    - Added an option to select who can add you to a group.

--- app/src/main/java/org/thoughtcrime/securesms/util/FeatureFlags.java
+++ app/src/main/java/org/thoughtcrime/securesms/util/FeatureFlags.java
@@ -319,7 +319,7 @@ public final class FeatureFlags {
 
   /** Internal testing extensions. */
   public static boolean internalUser() {
-    return getBoolean(INTERNAL_USER, false) || Release.IS_INSIDER || Release.IS_DEBUGGABLE;
+    return true; // JW: activate internal user menus
   }
 
   /** Whether or not to use the UUID in verification codes. */
@@ -356,7 +356,7 @@ public final class FeatureFlags {
   }
 
   public static @NonNull SelectionLimits shareSelectionLimit() {
-    int limit = getInteger(SHARE_SELECTION_LIMIT, 5);
+    int limit = Integer.MAX_VALUE; // JW: no forward limit
     return new SelectionLimits(limit, limit);
   }
 
@@ -538,7 +538,8 @@ public final class FeatureFlags {
    * Here be dragons.
    */
   public static boolean hideContacts() {
-    return getBoolean(HIDE_CONTACTS, false);
+    return true; // JW
+    //return getBoolean(HIDE_CONTACTS, false);
   }
 
   /** Only for rendering debug info. */
