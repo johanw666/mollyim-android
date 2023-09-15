531f49f5931a95f13d9a1b4a2dd91390e511f8ed
Johan <johanw@xs4all.nl>
2023-09-15 22:39:48 +0200

    Added extra options
    - Added encrypted and plaintext backup / restore.
    - Added WhatsApp import.
    - Option to prevent blocked users from adding you to a group.
    - Choice for the backup location.
    - Option to treat view-once media as normal media.
    - Option to ignore remote delete of messages.
    - Added an option to only delete media from a message, not the entire message.
    - Removed forward limit.
    - Show internal options. Use at your won risk.
    - Added an option to select who can add you to a group.

--- constants.gradle.kts
+++ constants.gradle.kts
@@ -2,6 +2,7 @@
 val signalBuildToolsVersion by extra("34.0.0")
 val signalCompileSdkVersion by extra("android-34")
 val signalTargetSdkVersion by extra(31)
-val signalMinSdkVersion by extra(24)
+// JW: changed minSDK from 24 to 23
+val signalMinSdkVersion by extra(23)
 val signalJavaVersion by extra(JavaVersion.VERSION_17)
 val signalKotlinJvmTarget by extra("17")
