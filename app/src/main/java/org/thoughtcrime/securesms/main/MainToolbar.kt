d6c04c334a4a92de609e3b5c9d719ceb1dd272ad
Johan <johanw@xs4all.nl>
2025-10-21 15:37:41 +0200

    Added extra options
    - Added encrypted and plaintext backup / restore.
    - Added WhatsApp import.
    - Choice for the backup location.
    - Option to treat view-once media as normal media.
    - Option to ignore remote delete of messages.
    - Added an option to only delete media from a message, not the entire message.
    - Removed forward limit.
    - Show internal options. Use at your won risk.
    - Added an option to select who can add you to a group (for non-linked devices).

--- app/src/main/java/org/thoughtcrime/securesms/main/MainToolbar.kt
+++ app/src/main/java/org/thoughtcrime/securesms/main/MainToolbar.kt
@@ -101,6 +101,7 @@ interface MainToolbarCallback {
   fun onCloseActionModeClick()
   fun onSearchQueryUpdated(query: String)
   fun onNotificationProfileTooltipDismissed()
+  fun onImportExportClick() // JW
 
   object Empty : MainToolbarCallback {
     override fun onNewGroupClick() = Unit
@@ -121,6 +122,7 @@ interface MainToolbarCallback {
     override fun onCloseActionModeClick() = Unit
     override fun onSearchQueryUpdated(query: String) = Unit
     override fun onNotificationProfileTooltipDismissed() = Unit
+    override fun onImportExportClick() = Unit // JW
   }
 }
 
@@ -585,6 +587,19 @@ private fun CallDropdownItems(callFilter: CallLogFilter, callback: MainToolbarCa
     )
   }
 
+  // JW: added
+  DropdownMenus.Item(
+    text = {
+      Text(
+        text = stringResource(R.string.arrays__import_export_orig)
+      )
+    },
+    onClick = {
+      callback.onImportExportClick()
+      onOptionSelected()
+    }
+  )
+
   DropdownMenus.Item(
     text = {
       Text(
@@ -675,6 +690,19 @@ private fun ChatDropdownItems(state: MainToolbarState, callback: MainToolbarCall
       }
     )
   }
+
+  // JW: added
+  DropdownMenus.Item(
+    text = {
+      Text(
+        text = stringResource(R.string.arrays__import_export_orig)
+      )
+    },
+    onClick = {
+      callback.onImportExportClick()
+      onOptionSelected()
+    }
+  )
 
   DropdownMenus.Item(
     text = {
